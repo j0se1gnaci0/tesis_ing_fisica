87e0e23fad8e1f0f1aa9c58137b9604fb76691b5
j0se1gnaci0 <gonzalezmoya.jose@gmail.com>
2018-06-04 17:31:02 -0400

    cambios

--- pelota/sphere.py
+++ pelota/sphere.py
@@ -7,28 +7,31 @@ from scipy.integrate import odeint
 
 def sistema(X,t):
 
-    m = 1
-    g = 10
-    kw = 1
+    #m = 1.0
+    g = 10.0
+    #kw = 1.0
 
     y   = X[0]
     dy  = X[1]
-    d2y = -m*g-kw*dy**2
+    d2y = g     #-kw*dy**2)/m
 
     return [dy,d2y]
 
 def proceso(t0,tf,y,vy,pasos):
 
-    t = np.arange(t0,tf,pasos)
+    t = np.linspace(t0,tf,pasos)
     Y = odeint(sistema,[y,vy],t,args=())
 
-    y = Y[:,0]
+    y  = Y[:,0] 
     vy = Y[:,1]
-    y = y[-1]
-    vy = vy[-1]
+    y  = y[-1]  #[m]
+    vy = vy[-1] #[m]
+    #y*=4389
+    #vy*=4.4
+    delta_t=tf-t0
 
-    t0 +=tf
-    tf +=tf
+    t0 +=delta_t
+    tf +=delta_t
     
     return y,vy,t0,tf
     
@@ -37,25 +40,25 @@ def caida_libre():
     reloj = pygame.time.Clock()
     FPS = 30
     pygame.init()
-    ancho=900
-    alto=900
+    ancho = 1000
+    alto = 10000
     
-    ventana=pygame.display.set_mode((alto,ancho))
+    ventana = pygame.display.set_mode((ancho,alto))
 
     pygame.display.set_caption("caida libre")
 
     #pelota = pygame.image.load('ball.jpg')
-    pelota=pygame.transform.scale(pygame.image.load('ball.jpg').convert_alpha(), (40,40))
+    pelota = pygame.transform.scale(pygame.image.load('ball.jpg').convert_alpha(), (40,40))
     blanco = pygame.Color(255,255,255)
     
-    x = ancho/2
+    x = 450
     y = 0
 
-    vy = 1
+    vy = 0
     
     t0 = 0
-    tf = 33.0/1000
-    pasos = 0.01
+    tf = 0.033
+    pasos = 10
     
     while True:
           ventana.fill(blanco)
@@ -71,8 +74,12 @@ def caida_libre():
                     pygame.quit()
                     sys.exit()
 
-          y,v,t0,tf = proceso(t0,tf,y,vy,pasos)
+          y,vy,t0,tf = proceso(t0,tf,y,vy,pasos)
+          #y=y*4389
+          #vy=vy*4.4
+          print (y,vy,tf)
           reloj.tick(FPS)
+    
           pygame.display.update()
 
 caida_libre()
